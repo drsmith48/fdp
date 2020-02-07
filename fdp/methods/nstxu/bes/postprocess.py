b8516bec0033e523df156d7aa4e071b23ec2b66e
David R. Smith <drsmith8@wisc.edu>
2020-02-07 15:14:53 -0500

    mods

---
+++ fdp/methods/nstxu/bes/postprocess.py
@@ -0,0 +1,6 @@
+from ....lib.utilities import isContainer
+
+def postprocess(self, *args, **kwargs):
+    print('in postprocess')
+    if isContainer(self) and self._name=='bes' and self.shot<200000:
+        print(type(self))
